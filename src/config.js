3159cd010f79b34da7ab4d340ae376ad4bf725c4
alexdascalu <dascalu.alex@yahoo.com>
2020-09-21 18:41:13 +0300

    Setting up React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-central-1",
+        BUCKET: "alex-notes-app-upload"
+    },
+    apiGateway: {
+        REGION: "eu-central-1",
+        URL: "https://jo7mbzz1je.execute-api.eu-central-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "eu-central-1",
+        USER_POOL_ID: "eu-central-1_6gZKpWvh0",
+        APP_CLIENT_ID: "4ue6ckudmkso7ufjmf0o3l7r8p",
+        IDENTITY_POOL_ID: "eu-central-1:2c5d71c9-a858-4120-8951-e49eba0d2805"
+    }
+};
